0adce7b8e4f8d3820b97d1bcd51b6f4d0e44058a
DxniChid <chidiroglou.danai06@gmail.com>
2026-02-05 15:02:06 +0100

    Prototype Job ad
    Created a prototype for job ads. This won't be final. Still in progress

---
+++ src/components/Job_Ad.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="jobs-wrapper">
+    <div
+      v-for="job in jobs"
+      :key="job.id"
+      class="job-card"
+    >
+      <div class="job-left">
+        <div class="job-title">{{ job.title }}</div>
+        <div class="job-company">{{ job.company }}</div>
+      </div>
+
+      <div class="job-right">
+        <div class="job-location">
+          📍 {{ job.city }}, {{ job.zip }} | {{ job.canton }}
+        </div>
+
+        <button
+          class="fav-btn"
+          :class="{ active: job.favorite }"
+          @click="$emit('toggle-favorite', job.id)"
+        >
+          ♥
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+defineProps({
+
+    jobs: {
+        type: Array,
+        required: true
+    }
+})
+
+defineEmits(["toggle-favorite"])
+</script>
+
+<style scoped>
+
+</style>

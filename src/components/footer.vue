7f7903e2687f1160367f20b5059881b0d28d922a
Patrick Imhof <patrick.imhof06@gmail.com>
2026-02-07 11:19:18 +0100

    Made few changes on footer

---
+++ src/components/footer.vue
@@ -0,0 +1,34 @@
+<script>
+    export default{
+        
+        props: {
+            contact:{
+                type: String,
+                required: true
+            },
+            adress:{
+                type: String,
+                required: true
+            }
+
+
+        }
+
+
+
+    }
+
+</script>
+
+<template>
+
+    <h4>{{ adress }}</h4>
+    <p>{{ adress }}</p>
+    <p>{{ adress }}</p>
+    <p>{{ adress }}</p>
+
+    <p>{{ contact }}</p>
+    <p>{{ contact }}</p>
+
+
+</template>

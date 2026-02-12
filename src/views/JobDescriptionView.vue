52c7fec784764eb3147b977f2581283f61f51ac4
Joseph <167524188+Joseph051006@users.noreply.github.com>
2026-02-12 16:02:59 +0100

    Continued on JobDescriptionView (WIP). I added an address component to the JobDescriptionView and styled it

--- src/views/JobDescriptionView.vue
+++ src/views/JobDescriptionView.vue
@@ -1,14 +1,22 @@
 <script setup>
 
 import JobDescription from '../components/description.vue'
+import Address from '../components/address.vue'
 
 </script>
 
 <template>
-    <div id="box">
 
-    <JobDescription jobtitle="Lorem Ipsum" description="Soem sugus", requirements="Sugus" offer="Pizza"/>
+
+    <div id="box">
+        <div id="description">
+            <JobDescription jobtitle="Lorem Ipsum" description="Soem sugus", requirements="Sugus" offer="Pizza"/>
+        </div>
+        <div id="address">
+            <Address location-icon="src/images/place-marker-icon.jpg", location="Visp" postal-code=3911 canton="VS" building-icon="src/images/building-icon.jpg" company-name="Informatik AG" ></Address>
+        </div>
     
+            
     </div>
 </template>
 
@@ -16,14 +24,25 @@ import JobDescription from '../components/description.vue'
 
     #box{
         display:flex;
-        justify-content: center;
-        flex-direction: column;
+        justify-content: space-between;
+        
+        flex-direction: row;
         background-color: #f6f5f4;
         padding-inline-start: 1%;
         margin-inline: 5%;
         border-radius: 30px;
         
     }
+    #description{
+        display: flex;
+        flex-direction: column;
+    }
+
+    #address{
+        padding-right: 3%;
+        padding-top: 1px;
+    }
+    
 
 </style>
 
